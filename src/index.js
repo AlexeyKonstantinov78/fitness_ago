98308853ea184d73881e5a1e45aa56267ab59c79
Aleksey Konstantinov <alex_kav@mail.ru>
2021-01-15 12:02:06 +0300

    главный слайдер

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import clubSelection from "./modules/clubSelection";
 import modalWindow from "./modules/modalWindow";
 import scrollWindow from "./modules/scrollWindow";
+import slider from "./modules/slider";
 
 //выпадающее меню выбор клуба
 clubSelection();
@@ -12,3 +13,6 @@ modalWindow();
 
 //стрелка scroll появление кнопки 
 scrollWindow();
+
+//Главный слайдер
+slider();
---
+++ src/modules/slider.js
@@ -0,0 +1,34 @@
+const slider = () => {
+
+    const mainSlider = document.querySelectorAll('.main-slider>.slide');
+
+    const changeSlider = (index) => {
+
+        mainSlider[index].style.display = 'none';
+
+        if (index !== mainSlider.length - 1) {
+            index++;
+            mainSlider[index].style.display = '';
+        } else {
+            index = 0;
+            mainSlider[index].style.display = '';
+        }
+
+        requestAnimationFrame(() => {
+            setTimeout(() => {
+                changeSlider(index);
+            }, 3000);
+        });
+    }
+
+    mainSlider.forEach((item, index) => {
+        if (item.style.display === '') {
+            setTimeout(() => {
+                changeSlider(index)
+            }, 3000);
+            return;
+        }
+    });
+}
+
+export default slider;
