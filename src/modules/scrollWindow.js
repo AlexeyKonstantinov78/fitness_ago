4803c71e3da7f94044c4ee928447ec3d882a832d
Aleksey Konstantinov <alex_kav@mail.ru>
2021-01-18 09:19:18 +0300

    бургер меню фиксация

--- src/modules/scrollWindow.js
+++ src/modules/scrollWindow.js
@@ -1,7 +1,18 @@
 const scrollWindow = () => {
-    const totop = document.getElementById('totop');
+    const totop = document.getElementById('totop'),
+        topMenu = document.querySelector('.top-menu');
+
     totop.style.display = 'none';
     window.addEventListener('scroll', () => {
+        console.log(outerWidth);
+        console.log(window);
+
+        if (pageYOffset > 181 && innerWidth < 768) {
+            topMenu.style.position = 'fixed';
+        } else {
+            topMenu.style.position = '';
+        }
+
         if (pageYOffset < 740) {
             totop.style.display = 'none';
         } else {
